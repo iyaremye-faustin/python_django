f0dc95326ae625d684a53dd5871d0f34af4b487a
Faustin IYAREMYE <iyaremyef@gmail.com>
2025-11-20 16:06:39 +0200

    fix

--- ticketing/settings.py
+++ ticketing/settings.py
@@ -78,9 +78,20 @@ WSGI_APPLICATION = 'ticketing.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'multiservices',
+        'USER': 'infinity',
+        'PASSWORD': 'infinityDBUser@me.com1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
